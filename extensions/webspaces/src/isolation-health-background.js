7653b4153a3a88f589e13d72fbccaf68abfdefe6
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-12 13:20:51 -0500

    fix(webspaces): isolate health message namespace

--- extensions/webspaces/src/isolation-health-background.js
+++ extensions/webspaces/src/isolation-health-background.js
@@ -1,8 +1,10 @@
 import { buildIsolationHealth } from "./isolation-health.js";
 import { loadConfig } from "./storage.js";
 
+const ISOLATION_HEALTH_MESSAGE = "webspaces-health:get-isolation-health";
+
 browser.runtime.onMessage.addListener((message) => {
-  if (message?.type !== "webspaces:get-isolation-health") return undefined;
+  if (message?.type !== ISOLATION_HEALTH_MESSAGE) return undefined;
 
   return Promise.all([
     loadConfig(),
